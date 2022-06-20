b7f41f27f4edf6dbea6c4c50aac78ea56bf95aaf
Nguyen Viet Hoang (APTECH HN) <hoangnvth2010033@fpt.edu.vn>
2022-06-20 21:08:56 +0700

    CRUD Tag done

--- ElevatorSystem.Admin/ElevatorSystem.Admin/Controllers/AdminController/BlogsController.cs
+++ ElevatorSystem.Admin/ElevatorSystem.Admin/Controllers/AdminController/BlogsController.cs
@@ -11,135 +11,132 @@ using ElevatorSystem.Admin.Models.Entity;
 
 namespace ElevatorSystem.Admin.Controllers.AdminController
 {
-    public class TagsController : Controller
+    public class BlogsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Tags
+        // GET: Blogs
         public ActionResult Index()
         {
-            return View(db.Tags.ToList());
+            var blogs = db.Blogs.Include(b => b.Tag);
+            return View(blogs.ToList());
         }
 
-        // GET: Tags/Details/5
+        // GET: Blogs/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Tag tag = db.Tags.Find(id);
-            if (tag == null)
+            Blog blog = db.Blogs.Find(id);
+            if (blog == null)
             {
                 return HttpNotFound();
             }
-            return View(tag);
+            return View(blog);
         }
 
-        // GET: Tags/Create
+        // GET: Blogs/Create
         public ActionResult Create()
         {
+            ViewBag.TagID = new SelectList(db.Tags, "ID", "Title");
             return View();
         }
 
-        // POST: Tags/Create
+        // POST: Blogs/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,Description,IsPublished,CreatedAt,ModifiedAt,PublishedAt")] Tag tag)
+        public ActionResult Create([Bind(Include = "ID,Title,Summary,IsPublished,PostContent,Thumbnail,Slug,CreatedAt,ModifiedAt,PublishedAt,TagID")] Blog blog)
         {
             if (ModelState.IsValid)
             {
-                tag.CreatedAt = DateTime.Today;
-                if(tag.PublishedAt <= DateTime.Today)
+                blog.CreatedAt = DateTime.Today;
+                if (blog.PublishedAt <= DateTime.Today)
                 {
-                    tag.IsPublished = true;
+                    blog.IsPublished = true;
                 }
                 else
                 {
-                    tag.IsPublished = false;
+                    blog.IsPublished = false;
                 }
-                db.Tags.Add(tag);
+                db.Blogs.Add(blog);
                 db.SaveChanges();
-                TempData["CreateMessage"] = "Tag #"+tag.ID+" '"+tag.Title + "' has been added to the list !";
                 return RedirectToAction("Index");
             }
 
-            return View(tag);
+            ViewBag.TagID = new SelectList(db.Tags, "ID", "Title", blog.TagID);
+            return View(blog);
         }
 
-        // GET: Tags/Edit/5
+        // GET: Blogs/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Tag tag = db.Tags.Find(id);
-            if (tag == null)
+            Blog blog = db.Blogs.Find(id);
+            if (blog == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.CheckStatus = tag.IsPublished;
-
-            return View(tag);
+            ViewBag.TagID = new SelectList(db.Tags, "ID", "Title", blog.TagID);
+            return View(blog);
         }
 
-        // POST: Tags/Edit/5
+        // POST: Blogs/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,Description,IsPublished,CreatedAt,ModifiedAt,PublishedAt")] Tag tag)
+        public ActionResult Edit([Bind(Include = "ID,Title,Summary,IsPublished,PostContent,Thumbnail,Slug,CreatedAt,ModifiedAt,PublishedAt,TagID")] Blog blog)
         {
             if (ModelState.IsValid)
             {
-                tag.ModifiedAt = DateTime.Today;
-                if (tag.PublishedAt <= DateTime.Today)
+                blog.ModifiedAt = DateTime.Today;
+                if (blog.PublishedAt <= DateTime.Today)
                 {
-                    tag.IsPublished = true;
+                    blog.IsPublished = true;
                 }
                 else
                 {
-                    tag.IsPublished = false;
+                    blog.IsPublished = false;
                 }
-                db.Entry(tag).State = EntityState.Modified;
+                db.Entry(blog).State = EntityState.Modified;
                 db.SaveChanges();
-                TempData["UpdateMessage"] = "Tag #" + tag.ID + " '" + tag.Title + "' has been updated !";
                 return RedirectToAction("Index");
             }
-            return View(tag);
+            ViewBag.TagID = new SelectList(db.Tags, "ID", "Title", blog.TagID);
+            return View(blog);
         }
 
-        // GET: Tags/Delete/5
+        // GET: Blogs/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Tag tag = db.Tags.Find(id);
-            if (tag == null)
+            Blog blog = db.Blogs.Find(id);
+            if (blog == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.CheckStatus = tag.IsPublished;
-           
-            return View(tag);
+            return View(blog);
         }
 
-        // POST: Tags/Delete/5
+        // POST: Blogs/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Tag tag = db.Tags.Find(id);
-           
-                db.Tags.Remove(tag);
-                db.SaveChanges();
-            TempData["DeleteMessage"] = "Tag #" + tag.ID + " '" + tag.Title + "' has been removed from the list !";
-            return RedirectToAction("Index"); 
+            Blog blog = db.Blogs.Find(id);
+            db.Blogs.Remove(blog);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
