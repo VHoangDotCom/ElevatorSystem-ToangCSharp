8142c1ccb8d95dff2497d755789d0be35ba935b2
@anhhvth2010043 <anhhvth2010043@fpt.edu.vn>
2022-06-20 22:26:28 +0700

    update-cors

--- ElevatorSystem.Admin/ElevatorSystem.Admin/App_Start/WebApiConfig.cs
+++ ElevatorSystem.Admin/ElevatorSystem.Admin/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ElevatorSystem.Admin
 {
@@ -11,6 +12,8 @@ namespace ElevatorSystem.Admin
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
